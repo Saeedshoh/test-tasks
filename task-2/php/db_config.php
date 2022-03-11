79d80f118f37b0b9f4b0f5739fa9a6fbacd3f8bd
Saeedshoh <52253289+Saeedshoh@users.noreply.github.com>
2022-03-11 17:24:36 +0500

    Update db_config.php

--- task-2/php/db_config.php
+++ task-2/php/db_config.php
@@ -2,9 +2,11 @@
 	$host = "localhost"; /* Host name */
 	$user = "root"; /* User */
 	$password = ""; /* Password */
-	$dbname = "tcell_db"; /* Database name */
+	$dbname = "task_db"; /* Database name */
 	
 	$con = mysqli_connect($host, $user, $password,$dbname);
+	mysqli_set_charset($con, 'utf8');
+    mysqli_query($con, "SET NAMES 'utf8'");
 	// Check connection
 	if (!$con) {
 	 die("Connection failed: " . mysqli_connect_error());
